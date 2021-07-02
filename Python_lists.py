08a607ae3c96df9161ec3204eef75e5da4c95584
Epicprogrammer1 <77883556+Epicprogrammer1@users.noreply.github.com>
2021-07-03 01:38:19 +0530

    Create Python_lists.py

---
+++ Python_lists.py
@@ -0,0 +1,32 @@
+"""
+Implement a python program to implement List operations (Nested List, Length, Concatenation, Membership, Iteration, Indexing and Slicing),
+"""
+
+#nested List 
+nestedList=[1,2,3,4,[5,6,7,8]]
+print("Length of nested List is : ", len(nestedList))
+
+#concatination example 
+a=[1,2,3]
+b=[4,5,6]
+print("a+b is ", a+b)
+
+#Membership Operator
+
+print("1 is present in list a", 1 in a)
+
+print("9 is present in list a", 9 in a)
+
+print("10 is not present in a ",10 not in a)
+
+#iteration 
+print("Elements of list a are: ")
+for i in a:
+    print(a)
+
+#Indexing
+print("Element at 0 and -1 index of 'a' are: ",a[0],a[-1])
+
+#slicing 
+c=a+b
+print("Slicing:",c[0:len(c):2])
